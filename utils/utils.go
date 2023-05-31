4dc9c121d0eb3167fa14eb06f02fef21075b67fa
Patrick Trindade <104721220+ptk-trindade@users.noreply.github.com>
2023-05-31 19:45:19 -0300

    Minor adjust on isPrime
    Previous version of isPrime was verbose and would return true for prime^2 numbers.

--- utils/utils.go
+++ utils/utils.go
@@ -18,15 +18,11 @@ func RandInt(n int) int {
 
 // ------ Prime ------
 
-func IsPrime(n int) bool {
-	if n <= 1 {
-		return false
-	}
-	
-	for i := 2; i*i < n; i++ {
-		if n % i == 0 {
+func isPrime(n int) bool {
+	for i := 2; i*i <= n; i++ {
+		if n%i == 0 {
 			return false
 		}
 	}
-	return true
+	return n > 1
 }
